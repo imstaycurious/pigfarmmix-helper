e1763097f72393baeb337119605985def7649d91
staycurious <276824702+imstaycurious@users.noreply.github.com>
2026-08-16 01:59:41 +0800

    fix: 移除 CSS 隐藏 tab 的规则

--- src/sw/sw.ts
+++ src/sw/sw.ts
@@ -8,7 +8,7 @@
 
 const swScope: any = globalThis;
 const swSelf: any = swScope;
-const CACHE = "pigfarm-v98";
+const CACHE = "pigfarm-v99";
 
 // 暴露版本号给主线程
 swSelf.addEventListener("message", (e: any) => {
